b1c6d45655c45cfee302e4ca16aa2a0e1b9eb610
Matt <pilon.matthieu@gmail.com>
2014-05-23 14:13:28 -0400

    prefab du perso

---
+++ Assets/Script/Camera/Parallax.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour {
+	private float x;
+	public int offset;
+	public bool follow;
+
+
+	// Use this for initialization
+	void Start () {
+		x = Camera.main.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (follow){
+			transform.position = new Vector2((Camera.main.transform.position.x - x) / offset,
+			Camera.main.transform.position.y);
+		}else{
+			transform.position = new Vector2((x - Camera.main.transform.position.x) / offset,
+			Camera.main.transform.position.y);
+		}
+
+	}
+}
