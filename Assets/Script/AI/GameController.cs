d7c09540742005aa59b441a8892d1f0e863bf658
Jean-Sébastien Gervais <jsgervais@hotmail.com>
2014-05-23 09:52:25 -0400

    timed spawnWave mechanics

--- Assets/Script/AI/GameController.cs
+++ Assets/Script/AI/GameController.cs
@@ -12,15 +12,17 @@ public class GameController : MonoBehaviour {
 	public Vector2 defaultSpawnPosition;  // defaut y range middel of simulated 2d floor;
 	public float minYSpawnPistionMinion, maxYSpawnPistionMinion;
 	public int minionSpawnCount;
+	public int minionMaxCount;
 
-	public float spawnWait; /// min time before next spawn wave. (spawns are also triggered)
+
+	public float minionSpawnWait; /// min time before next minion spawn in current wave. (spawns are also triggered)
 	public float startWait;
-	public float waveWait;
+	public float waveWait; /// min time before next spawn wave. (spawns are also triggered)
 
 
 	// Use this for initialization
 	void Start () {
-	
+		StartCoroutine (SpawnWaves ());
 	}
 	
 	// Update is called once per frame
@@ -33,15 +35,28 @@ public class GameController : MonoBehaviour {
 
 	IEnumerator SpawnWaves ()
 	{
+		GameObject[] minions;
+		int minionsToSpawn;
 		yield return new WaitForSeconds (startWait);
 		while (true)
 		{
-			for (int i = 0; i < minionSpawnCount; i++)
+			minionsToSpawn = minionSpawnCount; //
+			minions = GameObject.FindGameObjectsWithTag("minion");
+			if (minions!= null)
+			{
+				if (minionMaxCount - minions.Length < minionSpawnCount){
+					minionsToSpawn = minionMaxCount - minions.Length;
+				} 
+
+			}
+
+
+			for (int i = 0; i < minionsToSpawn; i++)
 			{
 				Vector2 spawnPosition = new Vector2 (defaultSpawnPosition.x, Random.Range (minYSpawnPistionMinion, maxYSpawnPistionMinion));
 				Quaternion spawnRotation = Quaternion.identity;
 				Instantiate (minionPrefab, spawnPosition, spawnRotation);
-				yield return new WaitForSeconds (spawnWait);
+				yield return new WaitForSeconds (minionSpawnWait);
 			}
 			yield return new WaitForSeconds (waveWait);
 		}
